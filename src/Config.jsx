3f2601df511c491521dc4380d3e804e81a47b9a8
surya <suryaprakashr1025@gmail.com>
2023-01-29 13:08:08 +0530

    push 29.1.2023 4

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,5 +1,5 @@
 export const Config = {
-    // api:"http://localhost:4000"
+    api:"http://localhost:4000"
     // api:"https://exam-backend-login.onrender.com"
-    api:"https://inventory-billing-kli1.onrender.com"
+    // api:"https://inventory-billing-kli1.onrender.com"
 }
